82e52d0c2b4b6b4eea595747254d5aac0f579b8d
MuhammadRay2 <151054422+MuhammadRay2@users.noreply.github.com>
2023-11-16 22:52:27 +0700

    Add files via upload

---
+++ ray.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ray
+ */
+public class ray {
+  public static void main (String[] args) {
+    String nama = "ray";
+    int umur = 18;
+    float tb = 174;
+    
+    String prodi = "Ilmu Komputer";
+    String nim = "I.2310138";
+    String kampus = "Universitas Djuanda Bogor";
+    
+    System.out.println("Assalamuallaikum Wr. Wb");
+    System.out.println("Nama saya " + nama);
+    System.out.println("Umur saya " + umur);
+    System.out.println("Tinggi saya " + tb);
+    
+    System.out.println("Saya dari prodi"+ prodi + "dengan NIM + nim");
+    System.out.println("Saya dari Universitas"+ kampus);
+    }
+}
